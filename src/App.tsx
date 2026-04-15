be687b6051bd12795aa90e99ccf94d6cd6ddfc1d
code-nightshade <cjsepttm@gmail.com>
2026-04-15 18:00:18 +0900

    타이머를 넣을 컴포넌트 기본 구조 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Footer from './components/footer';
 import Header from './components/header';
 import Home from './components/main/Home';
+import Ticket from './components/main/Ticket';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header /> {/* 상단 고정 */}
       <main>
         <Home />
+        <Ticket />
       </main>
       <Footer />
     </>
---
+++ src/components/main/Ticket.tsx
@@ -0,0 +1,9 @@
+export default function Ticket() {
+  return (
+    <div className='flex flex-col items-center' px-6>
+      <span className='text-[Poppins] text-[40px] text-[#EA5514] font-bold'>CARNIVAL START IN</span>
+      <span>Timer</span>
+      <button className='w-[150px] h-[51px] text-[Pretendard] bg-[#EA5514] text-black'>티켓 구매</button>
+    </div>
+  );
+}
